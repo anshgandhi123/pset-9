b368e8a4fde50ca2f59f679031a146291bfe3a09
Guest1 <my@email.comgit config --globak username my>
2020-03-01 17:58:00 -0500

    progress in js but only starting

---
+++ js/brickbreaker.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+let countingforWins = document.getElementById("headers")
+let speed;
+ctx.strokeStyle = "lightgreen";
+let changeSpeed;
+let start = false;
+let blocks = [];
+let wins = 0;
+let user = {
+    x: (canvas.width / 2) - 40,
+    y: canvas.height - 10,
+    width: 80,
+    height: 5,
+    movement: 20
+};
+
+let bounce = {
+    x: undefined,
+    y: undefined,
+    radius: 10,
+    right: true,
+    up: true
+};
+window.onload = function() {
+    document.getElementById("reset-button").onclick = init;
+    document.getElementById("game").onclick = init;
+    game();
+}
+document.addEventListener("keydown", getArrowKeys);
+function init() {
+    bounce.x = canvas.width / 2;
+    bounce.y = canvas.height - 20;
+    bounce.right = true;
+    bounce.up = true;
+    user.x = (canvas.width / 2) - 40;
+    user.y = canvas.height - 10;
+    changeSpeed = 1;
+    speed = 0;
+    blocks = [];
+    createblocks();
+    start = true;
+}
